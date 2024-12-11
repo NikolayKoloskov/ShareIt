7beaf8b7e7b8c6da8c5a70f9c95b1616dbd086a0
Nikolay Koloskov <koloskovnick@yandex.ru>
2024-12-12 00:47:35 +0300

    16-итерация 5 добавлены еще тесты

--- server/src/main/java/ru/practicum/shareit/booking/model/BookingState.java
+++ server/src/main/java/ru/practicum/shareit/booking/model/BookingState.java
@@ -10,12 +10,4 @@ public enum BookingState {
     FUTURE,
     ALL;
 
-    public static Optional<BookingState> from(String stringState) {
-        for (BookingState state : values()) {
-            if (state.name().equalsIgnoreCase(stringState)) {
-                return Optional.of(state);
-            }
-        }
-        return Optional.empty();
-    }
 }
